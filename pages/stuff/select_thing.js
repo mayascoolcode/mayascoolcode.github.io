db811b30f33690fb4fa6d2ead12cce11a50ff273
mayascoolcode <asboyd04@proton.me>
2025-12-31 09:50:29 -0800

    fix js files, add art

--- pages/stuff/select_thing.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('button[data-target]').forEach(btn => {
-  btn.onclick = function() {
-    const targetId = btn.getAttribute('data-target');
-    document.getElementById(targetId).style.display = '';
-  };
-});
-
