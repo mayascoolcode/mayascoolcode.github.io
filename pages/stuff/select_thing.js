22df431b4bb578688959108ddb02f2babd62fc31
mayascoolcode <asboyd04@proton.me>
2026-01-05 12:43:33 -0800

    add entry, remove sigil

---
+++ pages/stuff/select_thing.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('button[data-target]').forEach(btn => {
+    btn.onclick = function() {
+    const targetId = btn.getAttribute('data-target');
+    document.getElementById(targetId).style.display = '';
+    };
+});
+
